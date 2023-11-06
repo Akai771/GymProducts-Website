2b0f7638e4621d43521e8aec4f58dc13d190f2fc
Akai <rishabh.hs@somaiya.edu>
2023-11-06 17:50:17 +0530

    Update CartPage.jsx

--- gym/src/Components/CartPage/CartPage.jsx
+++ gym/src/Components/CartPage/CartPage.jsx
@@ -1,42 +1,125 @@
-import React from "react";
-import CartPageCard from "./CartPageCard";
-import CartPageData from "./CartPageData";
-import FooterNew from "../Footer/Footer";
-import CheckoutCard from "./CheckoutCard";
-import "./CartPage.css";
-import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function CartPage() {
-    var TotalValue = 0;
-    for(var i=0; i<CartPageData.length; i++) {  
-        TotalValue = TotalValue + parseInt(CartPageData[i].CartPagePrice);
-    }
-
-    return(<>
-        <h1 className="Mont800 hCart">Cart</h1>
-        <section className="CartPageAln">
-            <div className="CartCardAln">
-                {CartPageData.map((CartPageData) => (
-                  <CartPageCard key={CartPageData.id} CartPageData={CartPageData}/>
-                    ))
-                }
-            </div>
-            <div className="CheckoutCardAlnOut">
-                <div className="CheckoutCardAlnIn">
-                    <CheckoutCard/>
-                </div>
-                <div className="CouponAln">
-                    <LocalOfferOutlinedIcon style={{fontSize:30, transform: "scaleX(-1)", color:"#FFD369"}}/> 
-                    <span className="Mont600" style={{fontSize:"20px"}}> Apply Coupon</span>
-                    <div class="inputCoupon">
-                        <input type="text" class="CouponTxt" id="Coupon" name="Coupon" placeholder="Enter Coupon/Gift Code" autocomplete="off" />
-                        <input class="button--submit" value="Apply" type="submit" />
-                    </div>
-                </div>
-            </div>
-        </section>
-        <FooterNew />
-    </>)
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default CartPage;
+const defaultTheme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <br/> <br/>
+      </Container>
+    </ThemeProvider>
+  );
+}
